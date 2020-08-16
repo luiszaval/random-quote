db303572b56de27ec8bca1d08e11b60ac74f0335
Luis <lz24069@gmail.com>
2020-08-16 13:52:40 -0400

    Updates

---
+++ precache-manifest.a10123f4ca4c1ebd89ca9d1976f2977e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290aef358ccf909c4ffc4d44939653cb",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "2905aa46404c2846a3aa",
+    "url": "/random-quote/static/css/main.dd0fe759.chunk.css"
+  },
+  {
+    "revision": "d2f82a250ca858e8ca53",
+    "url": "/random-quote/static/js/2.0f252d38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote/static/js/2.0f252d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2905aa46404c2846a3aa",
+    "url": "/random-quote/static/js/main.0b9a8c53.chunk.js"
+  },
+  {
+    "revision": "8729920c30223b3cc616",
+    "url": "/random-quote/static/js/runtime-main.042e4bf7.js"
+  }
+]);
